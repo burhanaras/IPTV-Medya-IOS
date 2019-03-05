c3ea8b6fa8cf5829b838d26bc954055e9be4fcf9
tcbaras <Turkcell123>
2019-03-05 17:02:38 +0300

    Initial Commit

---
+++ IPTVMedya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IPTVMedya
+//
+//  Created by BURHAN ARAS on 5.03.2019.
+//  Copyright © 2019 BURHAN ARAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
